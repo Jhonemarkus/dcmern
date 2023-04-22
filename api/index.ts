c17f7e8d2adb6eafc688896d71f66241dc0a9b92
Marcos Moro <marcos.m@myplanet.com>
2023-04-22 03:48:53 -0400

    Convert api to TS

---
+++ api/index.ts
@@ -0,0 +1,24 @@
+import express, {Express, Request, Response} from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+const app: Express = express()
+dotenv.config()
+const port: number = parseInt(process.env.DCMERN_API_PORT || '3001')
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!!!')
+})
+
+app.get('/db', async (req: Request, res: Response) => {
+  try {
+    await mongoose.connect(process.env.DCMERN_DB_URL || '')
+    res.send('Connected to DB.')
+  } catch (e) {
+    res.send(`Error connecting to DB: ${e}`)
+  }
+})
+
+app.listen(port, () => {
+  console.log(`DCMERN-API listening on port ${port}`)
+})
